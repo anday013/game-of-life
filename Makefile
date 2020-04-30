67338c6ab54d58f55350dbd977693c2a7da6d6a3
Anday <48630069+anday2000@users.noreply.github.com>
2020-04-30 04:33:44 +0400

    Circular version implemented

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 CC = gcc
 
-all: sieve
+all: game.c helper.c point.c main.c
 
 
 clean: 
 		rm -f *.o sieve
+
+game: game.c helper.c point.c main.c
+	$(CC) $^ -o game
--- point.c
+++ point.c
@@ -120,32 +120,49 @@ int circular_live_neighbors(Point p, short field[SIZE][SIZE]){
         neighbors[7] = field[p.row - 1][0];  
     }
     else if(p.col == 0 && p.row == 0){
-        neighbors[0] = field[p.row][p.col + 1];
-        neighbors[1] = field[p.row + 1][p.col];
-        neighbors[2] = field[p.row + 1][p.col + 1];
+        neighbors[0] = field[0][1];
+        neighbors[1] = field[1][0];
+        neighbors[2] = field[1][1];
 
-        neighbors[3] = field[p.row + 1][p.col + 1];
-        neighbors[4] = field[p.row + 1][p.col + 1];
+        neighbors[3] = field[1][LIMIT];
+        neighbors[4] = field[LIMIT][1];
         neighbors[5] = field[LIMIT][LIMIT];       
-        neighbors[6] = field[LIMIT + 1][LIMIT];       
-        neighbors[7] = field[LIMIT][LIMIT - 1];  
+        neighbors[6] = field[LIMIT][0];       
+        neighbors[7] = field[0][LIMIT]  ;  
     }
     else if(p.col == LIMIT && p.row == 0){
-        neighbors[0] = field[p.row + 1][p.col];
-        neighbors[1] = field[p.row][p.col - 1];
-        neighbors[2] = field[p.row + 1][p.col - 1];
+        neighbors[0] = field[1][LIMIT];
+        neighbors[1] = field[0][LIMIT - 1];
+        neighbors[2] = field[1][LIMIT - 1];
+
+        neighbors[3] = field[0][0];
+        neighbors[4] = field[LIMIT][LIMIT - 1];
+        neighbors[5] = field[LIMIT][LIMIT];       
+        neighbors[6] = field[LIMIT][0];       
+        neighbors[7] = field[1][0];  
     }
     else if(p.col == 0 && p.row == LIMIT){
-        neighbors[0] = field[p.row - 1][p.col];
-        neighbors[1] = field[p.row][p.col + 1];
-        neighbors[2] = field[p.row - 1][p.col + 1];
+        neighbors[0] = field[LIMIT - 1][p.col];
+        neighbors[1] = field[LIMIT][1];
+        neighbors[2] = field[LIMIT - 1][1];
+
+        neighbors[3] = field[0][0];
+        neighbors[5] = field[LIMIT][LIMIT]; 
+        neighbors[4] = field[0][1];
+        neighbors[6] = field[LIMIT - 1][LIMIT];       
+        neighbors[7] = field[0][LIMIT]  ;  
     }
     else if(p.col == LIMIT && p.row == LIMIT){
         neighbors[0] = field[p.row][p.col - 1];
         neighbors[1] = field[p.row - 1][p.col];
         neighbors[2] = field[p.row - 1][p.col - 1];
+        neighbors[3] = field[0][0];
+        neighbors[4] = field[0][LIMIT - 1];
+        neighbors[5] = field[LIMIT][0]; 
+        neighbors[6] = field[LIMIT - 1][0];       
+        neighbors[7] = field[0][LIMIT]  ;  
     }
-    for(int i = 0; i < max_neighbors; i++)
+    for(int i = 0; i < MAX_NEIGHBORS; i++)
         if(neighbors[i]) alive++;
     return alive;
 }
@@ -159,7 +176,7 @@ int is_critical(Point p){
 int live_neighbors(Point p, short field[SIZE][SIZE]){
     int alive = 0;
     if(is_critical(p))
-        alive = clipped_live_neighbors(p,field);
+        alive = circular_live_neighbors(p,field);
     else{
         int neighbors[] = {
             field[p.row - 1][p.col],
