f0d08915bcf5dc34ba667069238517c16510aef0
Anday <48630069+anday2000@users.noreply.github.com>
2020-05-03 08:37:46 +0400

    argument bug fixed

--- main.c
+++ main.c
@@ -1,28 +1,29 @@
 #include "point.h"
 #include "game.h"
 #include "helper.h"
+#include <string.h>
 
-
-
-void main(int args, char **argv){
+void main(int args, char **argv)
+{
     short field[SIZE][SIZE];
-    if(args != 2){
+    char *s_version = argv[1];
+    if (args != 2)
+    {
         perror("Wrong arguments. Please check documentation");
         exit(0);
     }
-    if(argv[1] != "1" || argv[1] != "2"){
+    if (strcmp(s_version, "1") && strcmp(s_version, "2"))
+    {
         perror("Version can be 1 or 2");
         exit(0);
     }
-    int version = atoi(argv[1]);
+    int version = atoi(s_version);
     int stages = 10;
     initialize_field(field);
 
-    change_state(create_point(0,0),field);
-    change_state(create_point(1,0),field);
-    change_state(create_point(2,0),field);
+    change_state(create_point(0, 0), field);
+    change_state(create_point(1, 0), field);
+    change_state(create_point(2, 0), field);
 
-    
     start_game(stages, field, version);
 }
-
